f0d36853e41aab7abef52fd1962622b3f7ee938c
Rohit Karadkar <karadkar.rohit@gmail.com>
2017-03-24 16:50:57 +0530

    seperate screens

---
+++ app/src/main/java/io/github/karadkar/rohitdesigns/BottomSheetActivity.java
@@ -0,0 +1,63 @@
+package io.github.karadkar.rohitdesigns;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+public class BottomSheetActivity extends AppCompatActivity {
+    Context mContext;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_sheet);
+        mContext = getApplicationContext();
+
+        // get the bottom sheet view
+        LinearLayout llBottomSheet = (LinearLayout) findViewById(R.id.bottom_sheet);
+
+        // init behaviour
+        BottomSheetBehavior bottomSheetBehavior = BottomSheetBehavior.from(llBottomSheet);
+
+        // change state
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+
+        // add peek height
+//        bottomSheetBehavior.setPeekHeight(80);
+
+        // hideAble or not !
+        bottomSheetBehavior.setHideable(true);
+
+        bottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                switch (newState){
+                    case BottomSheetBehavior.STATE_EXPANDED:
+                        showToast("Expanded");
+                        break;
+                    case BottomSheetBehavior.STATE_COLLAPSED:
+                        showToast("Collapsed");
+                        break;
+                    /*case BottomSheetBehavior.STATE_DRAGGING:
+                        showToast("Dragging");
+                        break;*/
+                    case BottomSheetBehavior.STATE_HIDDEN:
+                        showToast("Closed");
+                        break;
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+
+            }
+        });
+    }
+    void showToast(String message){
+        Toast.makeText(mContext,message,Toast.LENGTH_SHORT).show();
+    }
+}
--- app/src/main/java/io/github/karadkar/rohitdesigns/MainActivity.java
+++ app/src/main/java/io/github/karadkar/rohitdesigns/MainActivity.java
@@ -1,63 +1,39 @@
 package io.github.karadkar.rohitdesigns;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomSheetBehavior;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ViewGroup.OnClickListener{
     Context mContext;
+    Button mBtnBottomSheet;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mContext = getApplicationContext();
-
-        // get the bottom sheet view
-        LinearLayout llBottomSheet = (LinearLayout) findViewById(R.id.bottom_sheet);
-
-        // init behaviour
-        BottomSheetBehavior bottomSheetBehavior = BottomSheetBehavior.from(llBottomSheet);
-
-        // change state
-        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
-
-        // add peek height
-//        bottomSheetBehavior.setPeekHeight(80);
-
-        // hideAble or not !
-        bottomSheetBehavior.setHideable(true);
-
-        bottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
-            @Override
-            public void onStateChanged(@NonNull View bottomSheet, int newState) {
-                switch (newState){
-                    case BottomSheetBehavior.STATE_EXPANDED:
-                        showToast("Expanded");
-                        break;
-                    case BottomSheetBehavior.STATE_COLLAPSED:
-                        showToast("Collapsed");
-                        break;
-                    /*case BottomSheetBehavior.STATE_DRAGGING:
-                        showToast("Dragging");
-                        break;*/
-                    case BottomSheetBehavior.STATE_HIDDEN:
-                        showToast("Closed");
-                        break;
-                }
-            }
-
-            @Override
-            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
-
-            }
-        });
+        findViewById(R.id.btn_bottom_sheet).setOnClickListener(this);
+        findViewById(R.id.btn_list_sample).setOnClickListener(this);
     }
-    void showToast(String message){
-        Toast.makeText(mContext,message,Toast.LENGTH_SHORT).show();
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_bottom_sheet:
+                startActivity(new Intent(mContext,BottomSheetActivity.class));
+                break;
+            case R.id.btn_list_sample:
+                startActivity(new Intent(mContext,ListActivity.class));
+                break;
+        }
     }
 }
