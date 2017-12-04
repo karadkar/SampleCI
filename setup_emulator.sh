68ace091fc7acc17d14f11d4a174b892692c1036
Rohit Karadkar <karadkar.rohit@gmail.com>
2017-12-05 00:20:57 +0530

    last try

--- setup_emulator.sh
+++ setup_emulator.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sdkmanager "system-images;android-25;google_apis;x86" && echo "no" | avdmanager create avd -n avd_25_x86 -k "system-images;android-25;google_apis;x86"
+sdkmanager "system-images;android-24;google_apis;armeabi-v7a" && echo "no" | avdmanager create avd -n avd_24_arm -k "system-images;android-24;google_apis;armeabi-v7a"
